8797294bd8de62801358cb7427a91c4acb8c787b
Michael Stegeman <michael@stegeman.me>
2018-09-05 11:49:45 -0600

    Cleanups.

--- index.js
+++ index.js
@@ -1,59 +1,61 @@
-
-
 const bodyParser = require('body-parser');
 const express = require('express');
-const app = express();
 const speech = require('@google-cloud/speech');
 
+const app = express();
+
 const config = {
-    port: process.env.PORT || 8000
+  port: process.env.PORT || 8000,
 };
 
 app.use(
-    bodyParser.raw({
-      limit: 1024000,
-      type: function () {
-        return true;
-      }
-    })
-  );
+  bodyParser.raw({
+    limit: 1024000,
+    type: () => true,
+  })
+);
 
-app.post('/asr', function (req, res, next) {
+app.post('/asr', (req, res) => {
+  // Creates a client
+  const client = new speech.SpeechClient();
 
-    // Creates a client
-    const client = new speech.SpeechClient();
+  // The audio file's encoding, sample rate in hertz, and BCP-47 language code
+  const audio = {
+    content: req.body.toString('base64'),
+  };
 
-    // The audio file's encoding, sample rate in hertz, and BCP-47 language code
-    const audio = {
-        content: req.body.toString('base64')
-    };
+  const config = {
+    encoding: 'LINEAR16',
+    sampleRateHertz: 16000,
+    languageCode: 'en-US',
+  };
 
-    const config = {
-        encoding: 'LINEAR16',
-        sampleRateHertz: 16000,
-        languageCode: 'en-US',
-    };
+  const request = {
+    audio: audio,
+    config: config,
+  };
 
-    const request = {
-        audio: audio,
-        config: config,
-    };
-
-    // Detects speech in the audio file
-    client
+  // Detects speech in the audio file
+  client
     .recognize(request)
-    .then(data => {
-        const response = data[0];
-        const result = '{"status":"ok","data":[{"confidence":'+ response.results[0].alternatives[0].confidence + ',"text":"' + response.results[0].alternatives[0].transcript + '"}]}';
-        res.status(200).send(result);
-        console.log('result 200' + `${response.results[0].alternatives[0].transcript} ` + Date.now());
-        return;
+    .then((data) => {
+      const response = data[0];
+      const result = response.results[0].alternatives[0];
+      console.log(`${Date.now()}: SUCCESS: ${result}`);
+      res.status(200).json({
+        status: 'ok',
+        data: [
+          {
+            confidence: result.confidence,
+            text: result.transcript,
+          },
+        ],
+      });
     })
-    .catch(err => {
-        console.log('result 500:', err, Date.now());
-        res.status(500).send('err');
-        return;
+    .catch((err) => {
+      console.log(`${Date.now()}: ERROR: ${err}`);
+      res.status(500).send('err');
     });
-})
+});
 
 app.listen(config.port);
